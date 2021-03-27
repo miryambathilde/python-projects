c83b0b517c7266052f77fbfc36dc9c4924d95d7f
Miryam Bathilde <mbathilde@gmail.com>
2021-03-27 12:20:01 +0100

    caracteres especiales, nueva linea y comandos

--- holamundo.py
+++ holamundo.py
@@ -3,3 +3,14 @@ print('Estoy aprendiendo Python')
 print('He visto la película "Los Vengadores"')
 
 print('Hola Python \nEstoy aprendiendo Python \nHe visto "Los Vengadores"')
+
+print('''
+Este es un string
+multilinea
+''')
+
+print('Voy a mostrar un \ carácter específico')
+
+print('Voy a mostrar un \n carácter específico')
+
+print('Voy a mostrar un \\n carácter específico')
