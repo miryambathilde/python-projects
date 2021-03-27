7b70e4fba04c87e53723506aa53391683330a9bb
Miryam Bathilde <mbathilde@gmail.com>
2021-03-27 12:11:56 +0100

    print + comando \n para nueva linea

--- holamundo.py
+++ holamundo.py
@@ -1,3 +1,5 @@
 print('Hola Python')
 print('Estoy aprendiendo Python')
 print('He visto la película "Los Vengadores"')
+
+print('Hola Python \nEstoy aprendiendo Python \nHe visto "Los Vengadores"')
