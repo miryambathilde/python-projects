71a399af9f48bbf1d72c1565853f4af35b5d25f9
Miryam Bathilde <mbathilde@gmail.com>
2021-03-27 13:40:35 +0100

    mejorando el input y la concatenacion

--- pelicula.py
+++ pelicula.py
@@ -1,5 +1,6 @@
-titulo = input ('Nombre de la película \n')
-director = input ('Nombre del director \n')
-actor_principal = input ('Nombre del actor principal \n')
+titulo = input ('Nombre de la película: \n')
+director = input ('Nombre del director: \n')
+actor_principal = input ('Nombre del actor principal: \n')
 
 print ('La película es: \n' + titulo + '\n' + 'El director de la película es: \n' + director + '\n' + 'El actor principal es: \n' + actor_principal)
+
